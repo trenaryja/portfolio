8b820a93a47801042b775cf16794a4fe942b85d8
Justin Trenary <trenaryja@gmail.com>
2025-08-06 00:07:54 -0400

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,22 @@ import Head from 'next/head'
 import { BackToTop, BackgroundParticles } from '../src/components'
 import { theme } from '../src/utils'
 
+import './globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <title>Justin Trenary: Portfolio</title>
-      </Head>
-      <ChakraProvider theme={theme}>
-        <BackgroundParticles />
-        <BackToTop />
-        <Box position="relative">
-          <Component {...pageProps} />
-        </Box>
-      </ChakraProvider>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<link rel='icon' type='image/svg+xml' href='/logo.svg' />
+				<title>Justin Trenary: Portfolio</title>
+			</Head>
+			<ChakraProvider theme={theme}>
+				<BackgroundParticles />
+				<BackToTop />
+				<Box position='relative'>
+					<Component {...pageProps} />
+				</Box>
+			</ChakraProvider>
+		</>
+	)
 }
